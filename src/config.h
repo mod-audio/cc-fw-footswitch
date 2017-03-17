b2059b82434dd3a5c30a7736dc9d3d0810a41437
Ricardo Crudo <ricardo.crudo@gmail.com>
2017-03-17 09:19:44 -0300

    increase version

--- src/config.h
+++ src/config.h
@@ -4,7 +4,7 @@
 // define firmware version
 #define CC_FIRMWARE_MAJOR   0
 #define CC_FIRMWARE_MINOR   0
-#define CC_FIRMWARE_MICRO   3
+#define CC_FIRMWARE_MICRO   4
 
 // maximum number of devices that can be created
 #define CC_MAX_DEVICES          1
