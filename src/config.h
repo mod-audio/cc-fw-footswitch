b2d7a00dc7d178d707aaa9a4038d7e99b2c913e9
Ricardo Crudo <ricardo.crudo@gmail.com>
2017-08-16 11:05:15 -0300

    update cc-slave and version

--- src/config.h
+++ src/config.h
@@ -4,7 +4,7 @@
 // define firmware version
 #define CC_FIRMWARE_MAJOR   0
 #define CC_FIRMWARE_MINOR   2
-#define CC_FIRMWARE_MICRO   1
+#define CC_FIRMWARE_MICRO   2
 
 // maximum number of devices that can be created
 #define CC_MAX_DEVICES          1
